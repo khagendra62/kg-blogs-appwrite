2f4fe6f697440249e65b0e56b178ac9c23e761c5
khagendra62 <khagendraawasthi47@gmail.com>
2025-12-03 09:37:00 +0545

    createPost

---
+++ src/appwrite/config.js
@@ -0,0 +1,33 @@
+import conf from "../conf/conf";
+import { Client, ID, Storage, Databases, Query } from "appwrite";
+
+export class Service {
+  client = new Client();
+  databases;
+  bucket;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteEndpoint)
+      .setProject(conf.appwriteProjectId);
+    this.databases = new Databases(this.client);
+    this.bucket = new Storage(this.client);
+  }
+
+  async createPost({ title, slug, content, featuredImage, status, userId }) {
+    try {
+      return await this.databases.createDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        { title, content, featuredImage, status, userId }
+      );
+    } catch (error) {
+      console.log("Appwrite serive :: createPost :: error", error);
+    }
+  }
+}
+
+const service = new Service();
+
+export default service;
