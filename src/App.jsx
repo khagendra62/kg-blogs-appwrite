a6d79ba835d9bc369c70e2eef3334689700b2aa9
khagendra62 <khagendraawasthi47@gmail.com>
2025-12-03 10:23:58 +0545

    change env variable name

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  const envVariable = import.meta.env.VITE_APPWRITE_URL;
+  const envVariable = import.meta.env.VITE_APPWRITE_ENDPOINT;
   return (
     <>
       <div className="text-center text-4xl bg-gray-500 text-green-500 font-black py-4 ">
