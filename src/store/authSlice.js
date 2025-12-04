f0912b800758e754f485a1933c92e45a3050266c
khagendra62 <khagendraawasthi47@gmail.com>
2025-12-04 06:01:04 +0545

    built store

---
+++ src/store/authSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  status: false,
+  userData: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.state = true;
+      state.userData = action.payload.userData; // or action.payload only bcz userData is same at name also
+    },
+
+    logout: (state) => {
+      //this also can access action but there is no need of action so i have excluded it
+      state.status = false;
+      state.userData = null;
+    },
+  },
+});
+
+export const { login, logout } = authSlice.actions;
+
+export default authSlice;
