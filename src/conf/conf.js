b5dfe154fae1729bc8d59e8ddc497a1822bf28d6
khagendra62 <khagendraawasthi47@gmail.com>
2025-12-16 09:01:26 +0545

    create services like createAccount, login, logout, currentUser

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,6 +1,7 @@
 const conf = {
   appwriteEndpoint: String(import.meta.env.VITE_APPWRITE_ENDPOINT),
   appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+  appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
   appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
   appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
 };
