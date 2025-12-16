6c5208f6669c06d67753a876bba933cbef70bfaa
khagendra62 <khagendraawasthi47@gmail.com>
2025-12-16 08:21:16 +0545

    setting up appwrite and env variable and use fonf for import export env variable as string(safe-side)

---
+++ src/conf/conf.js
@@ -0,0 +1,8 @@
+const conf = {
+  appwriteEndpoint: String(import.meta.env.VITE_APPWRITE_ENDPOINT),
+  appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+  appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+  appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+};
+
+export default conf;
