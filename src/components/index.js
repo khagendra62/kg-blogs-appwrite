9391648959ec91c65d4b0494e6bdc4f4c7aa2bd7
khagendra62 <khagendraawasthi47@gmail.com>
2025-12-17 13:36:03 +0545

    create login component

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,8 @@ import Footer from "./footer/Footer";
 import Container from "./container/Container";
 import Logo from "./ui/Logo";
 import LogoutBtn from "./ui/LogoutBtn";
+import Button from "./ui/Button";
+import Select from "./ui/Select";
+import PostCard from "./ui/PostCard";
 
-export { Header, Footer, Container, Logo, LogoutBtn };
+export { Header, Footer, Container, Logo, LogoutBtn, Button, Select, PostCard };
