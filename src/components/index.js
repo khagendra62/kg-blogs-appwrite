364412312220e2c7296328193942a0bfcae81aba
khagendra62 <khagendraawasthi47@gmail.com>
2025-12-17 10:16:23 +0545

    built reusable components btn, input and logoutbtn

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
+import Container from "./container/Container";
+import Logo from "./ui/Logo";
+import LogoutBtn from "./ui/LogoutBtn";
 
-export { Header, Footer };
+export { Header, Footer, Container, Logo, LogoutBtn };
---
+++ src/components/ui/Logo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Logo = ({ width = "100px" }) => {
+  return <div>KG BLOGS</div>;
+};
+
+export default Logo;
